a1da8c2431a37857f737188b16147125146d61e3
karthickkumarsm <sivakumarkarthickkumar@gmail.com>
2024-03-22 22:44:43 +0530

    v 1.3 - log feature & UI Update

--- lib/theme/theme_provider.dart
+++ lib/theme/theme_provider.dart
@@ -1,33 +1,52 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:habit_tracker/theme/dark_mode.dart';
 import 'package:habit_tracker/theme/light_mode.dart';
 
 class ThemeProvider extends ChangeNotifier {
-  
-  //initially light mode
-  ThemeData _themeData=lightMode;
+  // Initially light mode
+  ThemeData _themeData = lightMode;
 
-  //get current theme
+  // Key to store theme preference in shared preferences
+  static const String _themePreferenceKey = 'theme_preference';
+
+  // Constructor
+  ThemeProvider() {
+    // Initialize theme preference when the provider is created
+    _loadThemePreference();
+  }
+
+  // Get current theme
   ThemeData get themeData => _themeData;
 
-  //is current theme is dark theme
+  // Is current theme dark mode
   bool get isDarkMode => _themeData == darkMode;
 
-  //set theme
-  set themeData(ThemeData themeData){
-    _themeData =themeData;
+  // Set theme
+  set themeData(ThemeData themeData) {
+    _themeData = themeData;
+    _saveThemePreference(); // Save theme preference when theme changes
     notifyListeners();
   }
 
-  //toggle theme
-  void toggleTheme(){
-    if(_themeData == lightMode){
-      themeData=darkMode;
-    }
-    else{
-      themeData=lightMode;
+  // Toggle theme
+  void toggleTheme() {
+    themeData = isDarkMode ? lightMode : darkMode;
+  }
+
+  // Load theme preference from shared preferences
+  Future<void> _loadThemePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool? isDark = prefs.getBool(_themePreferenceKey);
+    if (isDark != null) {
+      _themeData = isDark ? darkMode : lightMode;
+      notifyListeners();
     }
   }
 
-  
+  // Save theme preference to shared preferences
+  Future<void> _saveThemePreference() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool(_themePreferenceKey, isDarkMode);
+  }
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,10 +9,12 @@ import awesome_notifications
 import awesome_notifications_core
 import isar_flutter_libs
 import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AwesomeNotificationsPlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsPlugin"))
   AwesomeNotificationsCorePlugin.register(with: registry.registrar(forPlugin: "AwesomeNotificationsCorePlugin"))
   IsarFlutterLibsPlugin.register(with: registry.registrar(forPlugin: "IsarFlutterLibsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
